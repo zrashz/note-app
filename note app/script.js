a73adaaa57376fd91ed008a2de238162ab17c7de
zrashz <152951952+zrashz@users.noreply.github.com>
2024-01-24 18:29:35 +0530

    Add files via upload

---
+++ note app/script.js
@@ -0,0 +1,41 @@
+const notecon=document.querySelector(".note-con");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notecon.innerHTML=localStorage.getItem("notes");
+}
+
+function updateStorage(){
+    localStorage.setItem("notes",notecon.innerHTML);
+}
+
+createBtn.addEventListener("click",()=>{
+    let inputBox=document.createElement("p");
+    let img =document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img .src="delete.png";
+    notecon.appendChild(inputBox).appendChild(img);
+})
+
+notecon.addEventListener("click",function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName=="p"){
+        notes.document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup=function(){
+                updateStorage();
+            }
+        })
+    }
+})
+document.addEventListener("keydown",event=>{
+    if(event.key=="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
